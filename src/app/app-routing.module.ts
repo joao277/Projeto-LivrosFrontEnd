c303dc3cfb9ad1c2968dad1d5b739e181646b218
Hiago Coelho <b8coelho@gmail.com>
2021-11-23 22:54:57 -0300

    [chore]: remove unused routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
 import { LivrosComponent } from './livros/livros.component';
 import { LoginComponent } from './login/login.component';
 import { RegislivroComponent } from './regislivro/regislivro.component';
-import { CriarcontaComponent } from './criarconta/criarconta.component';
 import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-  //{ path: 'home' , component: HomeComponent },
   { path: '' , component: LivrosComponent, canActivate: [AuthGuard] },
   { path: 'livros' , component: LivrosComponent, canActivate: [AuthGuard] },
   { path: 'regislivro' , component: RegislivroComponent, canActivate: [AuthGuard] },
-  { path: 'login' , component: LoginComponent },
-  { path: 'criarconta' , component: CriarcontaComponent}
+  { path: 'login' , component: LoginComponent }
 ];
 
 @NgModule({
